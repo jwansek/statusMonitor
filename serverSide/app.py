ae8a34861214b19610c35ecff21e3dc92fc8a06a
eden <eden@devuan>
2020-07-15 21:17:32 +0100

    started on js

--- serverSide/app.py
+++ serverSide/app.py
@@ -8,26 +8,41 @@ client = docker.from_env()
 
 @app.route("/")
 def hello_world():
-    return flask.jsonify("Hello World!")
+    return flask.render_template("index.html")
 
 @app.route("/sys")
 def get_sys_info():
     return flask.jsonify({
         "cpu_percent": psutil.cpu_percent(),
-        "temps": psutil.sensors_temperatures(),
-        "memory": psutil.virtual_memory()
+        "memory": psutil.virtual_memory(),
+        "swap": psutil.swap_memory(),
+        "storage": psutil.disk_usage("/")
     })
 
 @app.route("/docker")
 def get_docker_info():
-    out = {}
+    return flask.jsonify(get_docker_containers())
+
+def get_docker_containers():
+    out = []
     for container in client.containers.list():
-        print(container)
-    return flask.jsonify(out)
+        containerobj = json.loads(next(container.stats()).decode())
+        out.append({
+            "id": containerobj["id"],
+            "name": containerobj["name"],
+            "memory": {
+                "usage": containerobj["memory_stats"]["usage"],
+                "max": containerobj["memory_stats"]["limit"]
+            },
+            "cpu": containerobj["precpu_stats"]["cpu_usage"]["total_usage"],
+            "status": container.status,
+            "ports": container.ports
+        })
 
 
+    return out
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
+    # print(get_docker_containers())
 
-    print(get_docker_info())
---
+++ serverSide/static/index.js
@@ -0,0 +1,3 @@
+document.getElementById("nojs").style.display = 'none';
+console.log("Successfully Linked to JS");
+alert("foo");
